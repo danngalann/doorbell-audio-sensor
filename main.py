02603a2003876c47814d2e20ee814968ea724c46
danngalann <47415745+danngalann@users.noreply.github.com>
2023-03-11 13:26:20 +0100

    Continuously sample audio and determine if the doorbell was rung based on frequency and db

--- main.py
+++ main.py
@@ -1,12 +1,29 @@
 from utils.AudioManager import AudioManager
+import numpy as np
 
 
 def main():
     audio_manager = AudioManager()
-    frames = audio_manager.get_frames_from_file('audio/doorbell.wav')
-    audio_manager.cleanup()
 
-    print(audio_manager.get_frequencies(frames))
+    while True:
+        # Record one second of audio
+        frames = audio_manager.record(0.3)
+
+        # Get the frequencies from the audio
+        frequencies = audio_manager.get_frequencies(frames)
+
+        # Get the decibel value from the audio
+        decibels = audio_manager.get_decibels(frames)
+
+        # Calculate the averages of the frequencies
+        average_frequency = np.mean(frequencies)
+
+        if decibels > 60:
+            print(f"Average frequency: {average_frequency}")
+            print(f"Noise: {decibels}db")
+
+            if 19000 < average_frequency < 38000:
+                print("Doorbell rung!")
 
 
 # Press the green button in the gutter to run the script.
