20ecfc486696d94e67354d1bb27e150f5ef8b0cd
danngalann <47415745+danngalann@users.noreply.github.com>
2023-03-11 12:20:53 +0100

    Read files as frames. Add method to get frequencies

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ from utils.AudioManager import AudioManager
 
 def main():
     audio_manager = AudioManager()
-    audio_manager.play_from_file('audio/doorbell.wav')
+    frames = audio_manager.get_frames_from_file('audio/doorbell.wav')
     audio_manager.cleanup()
+
+    print(audio_manager.get_frequencies(frames))
 
 
 # Press the green button in the gutter to run the script.
--- utils/AudioManager.py
+++ utils/AudioManager.py
@@ -1,6 +1,7 @@
+import pyaudio
 import wave
 
-import pyaudio
+import numpy as np
 
 
 class AudioManager:
@@ -45,7 +46,7 @@ class AudioManager:
         stream.stop_stream()
         stream.close()
 
-    def play_from_file(self, filename):
+    def get_frames_from_file(self, filename):
         wf = wave.open(filename, 'rb')
         stream = self.p.open(format=self.p.get_format_from_width(wf.getsampwidth()),
                              channels=wf.getnchannels(),
@@ -54,14 +55,24 @@ class AudioManager:
                              frames_per_buffer=self.CHUNK)
         print("Playing back from file...")
 
+        frames = []
         data = wf.readframes(self.CHUNK)
         while data:
-            stream.write(data)
+            frames.append(data)
             data = wf.readframes(self.CHUNK)
 
         print("Done playing from file.")
         stream.stop_stream()
         stream.close()
+
+        return frames
+
+    def get_frequencies(self, frames):
+        samples = np.frombuffer(b''.join(frames), dtype=np.int16)
+        frequencies = np.fft.rfft(samples)
+        frequencies = abs(frequencies)
+
+        return frequencies
 
     def cleanup(self):
         self.p.terminate()
